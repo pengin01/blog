c68dd2685a055405f0f126b96af16928138a04b3
johnsmith0707 <misamisakondo@gmail.com>
2011-12-28 16:16:46 +0900

    model修正、デプロイファイル修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ActiveRecord::Base
   validates_presence_of :name,:title
-  validates_length_og :title >= 5
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 require "bundler/capistrano"
 set :application, "blog"
 set :repository,  "git://github.com/johnsmith0707/blog.git"
+set :deploy_to, "/tmp/capistrano_test"
+set :use_sudo, false
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -14,10 +16,16 @@ role :app, "192.168.50.3"                          # This may be the same as you
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do 
+    run "cd #{current_path} && bundle exec rails s -p 3000 -d"
+  end
+  task :stop do 
+    run "kill -9 `cat #{current_path}/tmp/pids/server.pid`"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+    #  run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
